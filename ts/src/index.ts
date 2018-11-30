e1be0ce7356da63d23397943f66fa8562485d507
Aaron Stevens <bheklilr2@gmail.com>
2018-11-30 14:45:42 -0600

    Set up CLI project for TS, seems to be working well

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,4 +1,28 @@
-import { part1, part2 } from "./day1";
+import Vorpal from 'vorpal'
 
-console.log(`${part1()}`);
-console.log(`${part2()}`);
+type DayModule = {
+    part1: () => void;
+    part2: () => void;
+}
+
+let cli = new Vorpal();
+cli.command("day [day]").action(async (args: Vorpal.Args) => {
+    if (args['day']) {
+        let day = "0" + args.day;
+        if (day.length == 3) {
+            day = day.substr(1);
+        }
+        let dayModule: DayModule = await import("./day" + day).catch((reason: any) => {
+            console.log("Enter a day between 1 and 25");
+        })
+        console.log(`Day ${day}, Part 1`);
+        console.log("=====================================================");
+        console.log(dayModule.part1());
+        console.log("\n\n");
+        console.log(`Day ${day}, Part 2`);
+        console.log("=====================================================");
+        console.log(dayModule.part2());
+        console.log();
+    }
+});
+cli.delimiter("aoc >").show();
